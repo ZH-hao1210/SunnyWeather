90eaeb562a3bc67c64db00a1dd318f5544ae5711
ZH-hao1210 <2426576056@qq.com>
2022-05-13 17:52:33 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/sunnyweather/db/City.java
@@ -0,0 +1,33 @@
+package com.example.sunnyweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
